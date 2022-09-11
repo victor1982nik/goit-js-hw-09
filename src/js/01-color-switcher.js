cfdddbafe0a64c77fffd52779e9d302d63dd6474
Victor Nikolenko <victor1982nik@gmail.com>
2022-09-11 20:11:34 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+btnStartRef = document.querySelector('button[data-start]');
+btnStopRef = document.querySelector('button[data-stop]');
+
+btnStartRef.addEventListener('click', handleBtnStartClick);
+btnStopRef.addEventListener('click', handleBtnStopClick);
+let timerId = null;
+
+function handleBtnStartClick() {    
+    timerId = setInterval(setBgColor, 1000);
+    
+    btnStartRef.style.opacity = 0.6;
+    btnStartRef.style.cursor = 'not-allowed';
+}
+
+
+function handleBtnStopClick() {
+    setBgColor('#fff');
+    clearInterval(timerId);
+    btnStartRef.style.opacity = 1;
+    btnStartRef.style.cursor = 'pointer';    
+}
+
+function setBgColor(color = getRandomHexColor()) {    
+    document.body.style.backgroundColor = color;    
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
