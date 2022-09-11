5134615ef0cc10a2b0eef3f7496edb08ddf18c1d
Victor Nikolenko <victor1982nik@gmail.com>
2022-09-12 00:18:14 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -87,7 +87,7 @@ function convertMs(ms) {
 function addLeadingZero(value) {
     return String(value).padStart('2',0)
 }
-
+/*
 function addLeadingZero(value) {
     return String(value).padStart('2',0)
-}
+}*/
