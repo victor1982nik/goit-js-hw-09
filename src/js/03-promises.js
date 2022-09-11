1d833498d12ea3cce1ce9a55f1b6de383e472b3d
Victor Nikolenko <victor1982nik@gmail.com>
2022-09-11 23:52:55 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', handlerFormSubmit);
+
+function createPromise(position, delay) {  
+    return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve(`Fullfiled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`Rejected promise ${position} in ${delay}ms`);
+    }
+    }, delay);
+  });    
+}  
+
+function handlerFormSubmit(e) {
+  e.preventDefault();
+  let delay = Number(formRef.elements.delay.value);
+  const step = Number(formRef.elements.step.value);
+  const amount = Number(formRef.elements.amount.value);     
+      
+    for (let i = 1; i <= amount; i += 1) {       
+    createPromise(i, delay)
+      .then(message =>  Notiflix.Notify.success(message))
+        .catch(message => Notiflix.Notify.failure(message));     
+      delay += step;
+  }    
 }
+
+
+
+
+
